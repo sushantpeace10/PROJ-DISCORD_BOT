31587c31ebda09c92150e45767dda96f43ad082a
SUSHANT CHAUHAN <51734530+sushantpeace10@users.noreply.github.com>
2021-07-28 07:50:07 +0000

    started mess.py and mess.json

---
+++ temp_new_cogs/mess.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+
+
+
+class Mess(commands.Cog):
+
+  def __init__(self, bot):
+    self.bot = bot
+
+ 
+
+
+
+
+
+
+def setup(bot):
+  bot.add_cog(Mess(bot))
